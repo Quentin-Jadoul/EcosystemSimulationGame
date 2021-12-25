8c578f65c546989964908e6976e57b1a471c541c
Quentin JADOUL <18155@ecam.be>
2021-12-25 16:07:47 +0100

    completed animal class

--- TrexRunner/Entities/Animal.cs
+++ TrexRunner/Entities/Animal.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using EcosystemSimulation.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EcosystemSimulation.Entities
 {
-    public class Animal
+    public class Animal : Living
     {
+        private const int TEXTURE_COORDS_X = 84;
+        private const int TEXTURE_COORDS_Y = 30;
+
+        private const int SPRITE_WIDTH = 39;
+        private const int SPRITE_HEIGHT = 36;
+
+        public Vector2 _position;
+
+        private Sprite _sprite;
+
+        public Animal(Texture2D spriteSheet, Vector2 position) : base(position)
+        {
+            _sprite = new Sprite(spriteSheet, TEXTURE_COORDS_X, TEXTURE_COORDS_Y, SPRITE_WIDTH, SPRITE_HEIGHT);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            _sprite.Draw(spriteBatch, Position);
+        }
     }
 }
